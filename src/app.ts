9154a20642d4cae6ee74f6712e2200809459584b
JuampaVLB <juampablovillalba25@gmail.com>
2023-04-17 20:55:22 -0300

    bug fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 // Modules
 
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
 
 const app: Application = express();
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/post', postRoutes);
 
 export default app;
