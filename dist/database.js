b3e8a8f3f7c6150fc9478d57686768a0a76b994a
JuampaVLB <juampablovillalba25@gmail.com>
2023-04-19 19:37:05 -0300

    bug fix .env in folder dist

--- dist/database.js
+++ dist/database.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const username = process.env.DB_USERNAME1;
+const password = process.env.DB_PASSWORD;
 const URI = `mongodb+srv://${username}:${password}@cluster0.ktoxf8e.mongodb.net/pet-tracker?retryWrites=true&w=majority`;
-// Localhost: mongodb://0.0.0.0:27017/pet-tracker
 mongoose_1.default
     .connect(URI)
     .then(_db => console.log('Database is connected'))
